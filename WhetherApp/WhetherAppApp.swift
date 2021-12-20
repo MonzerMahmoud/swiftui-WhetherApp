12ef3858257d00885c14dc22e71db6f2f304187b
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-12-20 11:28:16 +0200

    Initial Commit

---
+++ WhetherApp/WhetherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhetherAppApp.swift
+//  WhetherApp
+//
+//  Created by Syber Expertise on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WhetherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
