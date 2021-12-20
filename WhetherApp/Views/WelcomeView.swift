ecec8aa4914e43b841a8716c348916bb71da01e6
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-12-20 16:42:31 +0200

    Stable working version

---
+++ WhetherApp/Views/WelcomeView.swift
@@ -0,0 +1,40 @@
+//
+//  WelcomeView.swift
+//  WhetherApp
+//
+//  Created by Syber Expertise on 20/12/2021.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the weather app")
+                    .bold().font(.title)
+                
+                Text("Please share your current location to get the weather in ypur area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
